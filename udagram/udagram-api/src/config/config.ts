e3230726fd6c61982b2e4d128ce15b9c6d3c7382
aaboud@ejada.com <aaboud@ejada.com>
2025-07-31 22:54:23 +0300

    Add CircleCI pipeline

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -1,10 +1,31 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-// ENV variables 
+// ENV variables
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
+
+// export const config = {
+//   username: `${process.env.POSTGRES_USERNAME}`,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: process.env.POSTGRES_DB,
+//   host: process.env.POSTGRES_HOST,
+//   aws_region: process.env.AWS_REGION,
+//   aws_profile: process.env.AWS_PROFILE,
+//   aws_media_bucket: process.env.AWS_BUCKET,
+//   url: process.env.URL,
+//   jwt: {
+//     secret: process.env.JWT_SECRET,
+//   },
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// };
 
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
@@ -17,5 +38,13 @@ export const config = {
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
+  },
+  dialect: "postgres",
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // في بيئة التطوير فقط
+    },
   },
 };
--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,5 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+# aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
+# aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+
+aws s3 cp --recursive --acl public-read ./dist s3://myawsbucket-01155187731
+# aws s3 cp --recursive --acl public-read ./build s3://myawsbucket-768300774983/
