002b23025415adfab26621caab33ed7c9a1f18e6
aaboud@ejada.com <aaboud@ejada.com>
2025-07-31 19:22:50 +0300

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,32 @@
+# # This file is used for convenience of local development.
+# # DO NOT STORE YOUR CREDENTIALS INTO GIT
+# export POSTGRES_USERNAME=postgres
+# export POSTGRES_PASSWORD=myPassword
+# export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+# export POSTGRES_DB=postgres
+# export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+# export AWS_REGION=us-east-1
+# export AWS_PROFILE=default
+# export JWT_SECRET=mysecretstring
+# export URL=http://localhost:8100
+
+
+
+# Database (PostgreSQL RDS)
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=aya123456
+export POSTGRES_DB=postgres
+export POSTGRES_HOST=mypostgresdb.cpla9ldaujwx.us-east-1.rds.amazonaws.com
+export POSTGRES_PORT=5432
+
+# AWS S3
+export AWS_BUCKET=myawsbucket-01155187731
+export AWS_REGION=us-east-1
+
+# AWS IAM
+export AWS_PROFILE=default  # أو اسم البروفايل في ~/.aws/credentials
+
+# Application settings
+# export JWT_SECRET=71b34fc6084624ee000505bd83471679e9d37f37185107d326b2817b8a433a1c
+export JWT_SECRET=e0c03b97d38c1ae6fe58b2a8f09a2d9e110bff1037f42bb9c6f19318c3b3e7f1
+export URL=http://localhost:8100
