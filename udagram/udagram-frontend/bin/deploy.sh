364ab794b43b17391089571a177fa8a76ef4fc75
aaboud@ejada.com <aaboud@ejada.com>
2025-08-01 16:10:52 +0300

    Make change

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,5 +1,8 @@
 # aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
 # aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+cd udagram/udagram-frontend
+npm install
+npm run build
 
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-01155187731
+aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-01155187731/
 # aws s3 cp --recursive --acl public-read ./build s3://myawsbucket-768300774983/
